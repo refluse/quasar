31d0f4fce975a087b6e9ae402712b2df415a99d0
Arlina Design <Arlina-Design@users.noreply.github.com>
2022-08-02 20:35:32 +0700

    Create lazyadsense.js

---
+++ lazyadsense.js
@@ -0,0 +1,1 @@
+!function(){var a=document.createElement("script"),b=document.createAttribute("data-ad-client");b.value="ca-pub-1619167471144570",a.setAttributeNode(b),a.defer=!0,a.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";var c=document.getElementsByTagName("head")[0];c.parentNode.insertBefore(a,c)}();
